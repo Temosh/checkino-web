831d0d1157010ac9c6d74f782b516d89462b950b
Serhii Tymoshenko <temosh@i.ua>
2024-01-25 18:54:31 +0700

    ESList settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,23 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    // '@typescript-eslint/no-unused-vars': [
+    //   'error',
+    //   { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    // ],
   },
-}
+};
